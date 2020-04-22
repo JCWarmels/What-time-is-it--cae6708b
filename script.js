532d2c33605e68a4f143fb259acc3f5e1d8881d1
JCWarmels <jesse.warmels@student.rocva.nl>
2020-04-22 14:02:02 +0200

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function trying()
 {  
     function calculatingPerimeter() 
     {   
-        var diameter = document.querySelector("input#diameter").value;
-        perimeter = diameter * Math.PI;
+        diameter = document.querySelector("input#diameter").value;
+        perimeter = (diameter * Math.PI).toFixed(2);
         return {
             hit: (diameter>0),
             value: perimeter
@@ -13,9 +13,14 @@ function trying()
         throw calculatingPerimeter();
     } catch (data) {
         element =  document.getElementById("show_output");
-        value = data.value;
+        var value = data.value;
         if (data.hit || (perimeter/Math.PI) < diameter + 1 && (perimeter/Math.PI) > diameter - 1) {
-            element.innerHTML = "The perimeter is the following: " + value + " centimeter. <br> It would look like this:";
+            element.innerHTML = "The perimeter is the following: " + value + " centimeter.";
+            if (true) {
+                let value = (diameter * diameter * Math.PI * 0.25).toFixed(2);
+                element.innerHTML += "<br> The surface is the following: " + value + " centimeter.";
+            }
+            element.innerHTML += "<br> It would look like this:";
             draw(value);
         } else {
             element.innerHTML = "Something went wrong... That's all we know.";
@@ -37,4 +42,23 @@ function trying()
         ctx.stroke();
         }
     }
+}
+
+function justSetAnAlarm() 
+{
+    var current_time = new Date();
+    var current_hours = current_time.getHours();
+    var current_minutes = current_time.getMinutes();
+    var current_seconds = current_time.getSeconds();
+
+    current_minutes = (current_minutes < 10 ? "0" : "") + current_minutes;
+    current_seconds = (current_seconds < 10 ? "0" : "") + current_seconds;
+
+    var time_of_day = (current_hours < 12) ? "AM" : "PM";
+    current_hours = (current_hours > 12) ? current_hours - 12 : current_hours;
+    current_hours = (current_hours == 0) ? 12 : current_hours;
+
+    var current_time_string = current_hours + ":" + current_minutes + ":" + current_seconds + " " + time_of_day;
+
+    document.getElementById("clock").firstChild.nodeValue = current_time_string;
 }
